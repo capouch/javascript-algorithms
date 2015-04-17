0a92acee35c8d4316b0bb5c57e4ad6240e0c14c9
Brian Capouch <brianc@saintjoe.edu>
2015-04-17 13:50:01 -0400

    Call heapsort to show how Node does it

---
+++ testHeap.js
@@ -0,0 +1,25 @@
+ function dispArr(arr) {
+  for (var i = 0; i < arr.length; ++i) {
+    process.stdout.write(arr[i] + " ");
+    if (i % 10 == 9) {
+      process.stdout.write("\n");
+      }
+    }
+  if (i % 10 != 0) {
+    process.stdout.write("\n");
+    }
+ }
+
+var MAX = 50
+var sort = require('./src/sorting/heapsort.js').heapSort;
+var values = [];
+for (var i = 0; i < MAX; ++i) {
+  values[i] = Math.floor(Math.random() * MAX+1);
+  }
+
+console.log('\nBefore: ');
+dispArr(values);
+console.log('\nAfter: ');
+
+dispArr(sort(values)); 
+
